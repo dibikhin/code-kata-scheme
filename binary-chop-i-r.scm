023fd15f9a0bb75c4030446830653723115ea6c0
Roman Dibikhin <r.dibikhin@gmail.com>
2013-06-02 15:40:10 +0400

    working version, few tests

--- binary-chop-i-r.scm
+++ binary-chop-i-r.scm
@@ -1,4 +1,4 @@
-﻿; test-kit
+; test-kit
 (define vect-len vector-length)
 
 (define vect-ref vector-ref)
@@ -11,33 +11,31 @@
 (define (assert-equal expected actual)
   (if (= actual expected)
       "OK"
-      (begin
-    (display actual)
-    (display " is not equal to ")
-    (display expected)
-    (newline) "bad...")))
+      (begin (display actual)
+             (display " is not equal to ")
+             (display expected)
+             (newline) "bad...")))
 
 ; binary chop: itarative process, recursive calls
 (define (chop-iter num array buttom top)
   (if (> buttom top)
       -1
-      (let ((middle (/ (+ top buttom) 2)))
-            middle)))
-            
-			
-; (let ((middle-num (vect-ref array middle)))
-                  ; (if (= middle-num num)
-				      ; middle))			
-				  
+      (let ((middle (/ (+ top buttom) 2))) ;integer required !
+           (let ((middle-num (vect-ref array middle)))
+                (cond ((= middle-num num) middle)
+                      ((< middle-num num) (chop-iter num array (+ middle 1) top))
+                      ((< num middle-num) (chop-iter num array buttom (- middle 1))))))))
+          
 (define (chop num array)
   (if (vect-empty? array)
       -1
       (chop-iter num array 0 (- (vect-len array) 1))))
 
 (define (test-chop)
-  (begin
-    (assert-equal -1 (chop 3 #()))
-    (assert-equal -1 (chop 4 #(1))
-    (assert-equal -1 (chop 2 #(5))))))
+  (begin (assert-equal -1 (chop 3 #()))
+         (assert-equal -1 (chop 4 #(1)))
+         (assert-equal -1 (chop 2 #(5)))
+         (assert-equal  0 (chop 1 #(1)))
+         (assert_equal -1 (chop 0 #(1 3)))))
 
 (test-chop)
