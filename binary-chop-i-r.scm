b85b08f1794fae4e43e6d08eb7894b64ae66a354
Roman Dibikhin <r.dibikhin@gmail.com>
2013-06-02 22:28:58 +0400

    fixnum bug fixed, all tests added

--- binary-chop-i-r.scm
+++ binary-chop-i-r.scm
@@ -20,7 +20,7 @@
 (define (chop-iter num array buttom top)
   (if (> buttom top)
       -1
-      (let ((middle (/ (+ top buttom) 2))) ;integer required !
+      (let ((middle (truncate (/ (+ top buttom) 2))))
            (let ((middle-num (vect-ref array middle)))
                 (cond ((= middle-num num) middle)
                       ((< middle-num num) (chop-iter num array (+ middle 1) top))
@@ -31,11 +31,42 @@
       -1
       (chop-iter num array 0 (- (vect-len array) 1))))
 
+; test asserts
 (define (test-chop)
-  (begin (assert-equal -1 (chop 3 #()))
-         (assert-equal -1 (chop 4 #(1)))
-         (assert-equal -1 (chop 2 #(5)))
+  (begin (assert-equal -1 (chop 3 #()))           
+         (assert-equal -1 (chop 3 #(1)))
+         (assert-equal -1 (chop 3 #(4)))
          (assert-equal  0 (chop 1 #(1)))
-         (assert_equal -1 (chop 0 #(1 3)))))
+    
+         (assert-equal  0 (chop 1 #(1 3)))
+         (assert-equal  1 (chop 3 #(1 3)))
+         (assert-equal -1 (chop 4 #(1 3)))
+         (assert-equal -1 (chop 0 #(1 3)))
+    
+         (assert-equal  0 (chop 1 #(1 3 5)))
+         (assert-equal  1 (chop 3 #(1 3 5)))
+         (assert-equal  2 (chop 5 #(1 3 5)))
+         (assert-equal -1 (chop 0 #(1 3 5)))
+         (assert-equal -1 (chop 2 #(1 3 5)))
+         (assert-equal -1 (chop 4 #(1 3 5)))
+         (assert-equal -1 (chop 6 #(1 3 5)))
+   
+         (assert-equal  0 (chop 1 #(1 3 5 7)))
+         (assert-equal  1 (chop 3 #(1 3 5 7)))
+         (assert-equal  2 (chop 5 #(1 3 5 7)))
+         (assert-equal  3 (chop 7 #(1 3 5 7)))
+         (assert-equal -1 (chop 0 #(1 3 5 7)))
+         (assert-equal -1 (chop 2 #(1 3 5 7)))
+         (assert-equal -1 (chop 4 #(1 3 5 7)))
+         (assert-equal -1 (chop 6 #(1 3 5 7)))
+         (assert-equal -1 (chop 8 #(1 3 5 7)))
+  
+         (assert-equal -1 (chop 8 #(1 3 5 7 9)))
+         (assert-equal -1 (chop 6 #(1 3 5 7 9 11)))
+         (assert-equal -1 (chop 8 #(1 3 5 7 9 11)))
+         (assert-equal -1 (chop 10 #(1 3 5 7 9 11)))
+         (assert-equal -1 (chop 12 #(1 3 5 7 9 11)))
+  
+         (assert-equal -1(chop 4 #(1 3 5 7 9 11 13)))))
 
 (test-chop)
