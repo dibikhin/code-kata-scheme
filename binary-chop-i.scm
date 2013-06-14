d0d95512d07f4c7d5396d59ea395634d83ae6940
dibikhin <dibikhin@gmail.com>
2013-06-14 22:43:38 +0400

    full iterative version added

--- binary-chop-i.scm
+++ binary-chop-i.scm
@@ -1,41 +1,49 @@
 ; test-kit
-(define vect-len vector-length)
+(define (assert-equal expected actual)
+  (if (= actual expected)
+      (begin (display "; OK") 
+             (newline)
+             "; ALL DONE")
+      (begin (display "; ERROR. Expected ")
+             (display expected)
+             (display " but was ")
+             (display actual)
+             (newline) ". Bad...")))
 
+; helpers
+(define nil '())
+
+(define vect-len vector-length)
 (define vect-ref vector-ref)
 
 (define (vect-empty? array)
   (if (= (vect-len array) 0)
       #t
       #f))
-  
-(define (assert-equal expected actual)
-  (if (= actual expected)
-      (begin (newline) 
-             (display "; OK") 
-             "; DONE")
-      (begin (display actual)
-             (display " is not equal to ")
-             (display expected)
-             (newline) "bad...")))
 
-; binary chop: itarative process, recursive calls
-(define (chop-iter num array buttom top)
-  (if (> buttom top)
-      -1
-      (let ((middle (truncate (/ (+ top buttom) 2))))
-           (let ((middle-num (vect-ref array middle)))
-                (cond ((= middle-num num) middle)
-                      ((< middle-num num) (chop-iter num array (+ middle 1) top))
-                      ((< num middle-num) (chop-iter num array buttom (- middle 1))))))))
-
+; binary chop: itarative version
 (define (chop num array)
-  (if (vect-empty? array)
-      -1
-      (chop-iter num array 0 (- (vect-len array) 1))))
+   (if (vect-empty? array)
+       -1
+       (do ((bottom 0)
+            (top (- (vect-len array) 1))
+            (middle nil)
+            (found-pos nil)
+            (middle-num nil))
+           ((> bottom top)
+            (if (null? found-pos) 
+                -1
+                found-pos))       
+          (set! middle (truncate (/ (+ top bottom) 2)))
+          (set! middle-num (vect-ref array middle))        
+          (cond ((= middle-num num) (set! found-pos middle) (set! top -2)) ; let u burn in hell!
+                ((< middle-num num) (set! bottom (+ middle 1)))
+                ((< num middle-num) (set! top (- middle 1)))))))
 
 ; test asserts
 (define (test-chop)
-  (begin (assert-equal -1 (chop 3 #()))           
+  (begin 
+         (assert-equal -1 (chop 3 #()))           
          (assert-equal -1 (chop 3 #(1)))
          (assert-equal -1 (chop 3 #(4)))
          (assert-equal  0 (chop 1 #(1)))
@@ -69,6 +77,6 @@
          (assert-equal -1 (chop 10 #(1 3 5 7 9 11)))
          (assert-equal -1 (chop 12 #(1 3 5 7 9 11)))
   
-         (assert-equal -1(chop 4 #(1 3 5 7 9 11 13)))))
+         (assert-equal -1 (chop 4 #(1 3 5 7 9 11 13)))))
 
 (test-chop)
