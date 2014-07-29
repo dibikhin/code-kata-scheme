c9d4822ec383aaa54b37b349f493bc6bae71991b
dibikhin <dibikhin@gmail.com>
2014-07-29 21:24:57 +0400

    first draft

---
+++ Kata Four/min-spread-day-num.scm
@@ -0,0 +1,15 @@
+﻿(define (min-spread-day-num data) 
+    (day-num
+        (min-spread-entry data)))
+
+(define (day-num entry)
+    ())
+    
+(define (min-spread-entry data)
+    (min spread data))
+
+(define (spread entry)
+    (abs
+        (- 
+            (max-temp entry)
+            (min-temp entry))))
