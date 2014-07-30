b5a147a5eca4bcfa524c2d923e709e990aeb04ca
dibikhin <dibikhin@gmail.com>
2014-07-30 21:40:58 +0400

    vector usage & test added

--- Kata Four/min-spread-day-num.scm
+++ Kata Four/min-spread-day-num.scm
@@ -1,11 +1,11 @@
 ﻿(define (id entry)
-    ())
+    (vector-ref vector 0))
 
 (define (max-val entry)
-    ())
+    (vector-ref vector 1))
 
 (define (min-val entry)
-    ())
+    (vector-ref vector 2))
     
 (define (spread entry)
     (abs
@@ -13,18 +13,19 @@
             (max-val entry)
             (min-val entry))))
 
-(define (find-min spread entry-set) 
-	(vector-first
-		(vector-sort
-			(vector-map spread entry-set))))
+(define (vector-first vector)
+	(vector-ref vector 0))
 
-(define (min-spread-entry entry-set)
-    (find-min spread entry-set))
-
-(define (min-spread-id entry-set) 
+(define (min-spread-id entry-set)
     (id
-        (min-spread-entry entry-set)))
+		(vector-first
+			(vector-sort
+				(vector-map spread entry-set)))))
 
-; 1 88 59
-; 2 79 63
-; 3 77 55
+(define entry-set 
+	#(
+		#(1 88 59)
+		#(2 79 63)
+		#(3 77 55)))
+
+(min-spread-id entry-set)
