d33a2241c55be8136311c382278cf0168b0097b6
dibikhin <dibikhin@gmail.com>
2014-07-31 11:45:29 +0400

    pt.1 completed & tested, all is fine

--- Kata Four/min-spread-day-num.scm
+++ Kata Four/min-spread-day-num.scm
@@ -1,31 +1,78 @@
-﻿(define (id entry)
-    (vector-ref vector 0))
+﻿; tested in MIT/GNU Scheme only
 
-(define (max-val entry)
-    (vector-ref vector 1))
+; data structs due to #(3 77 55)
+(define (id vect)
+    (vector-ref vect 0))
 
-(define (min-val entry)
-    (vector-ref vector 2))
+(define (max-val vect)
+    (vector-ref vect 1))
+
+(define (min-val vect)
+    (vector-ref vect 2))
     
-(define (spread entry)
+(define (spread vect)
     (abs
         (- 
-            (max-val entry)
-            (min-val entry))))
+            (max-val vect)
+            (min-val vect))))
 
-(define (vector-first vector)
-	(vector-ref vector 0))
+; vector helpers
+(define (vector-first vect)
+	(vector-ref vect 0))
 
-(define (min-spread-id entry-set)
-    (id
+(define (vect-empty? array)
+  (if (= (vect-len array) 0)
+      #t
+      #f))
+
+; main worker
+(define (vector-sort-by get-value vect-of-vects)
+	(sort vect-of-vects
+		(lambda (x y)
+			(let 
+				((x-value (get-value x)) 
+				 (y-value (get-value y)))
+				(cond 
+					((= x-value y-value) 0)
+					((< x-value y-value) #t)
+					((> x-value y-value) #f))))))
+
+(define (min-spread-id vect-of-vects)
+	(id
 		(vector-first
-			(vector-sort
-				(vector-map spread entry-set)))))
+			(vector-sort-by spread vect-of-vects))))
 
 (define entry-set 
 	#(
 		#(1 88 59)
 		#(2 79 63)
-		#(3 77 55)))
+		#(3 77 55)
+		#(4 77 59)
+		#(5 90 66)
+		#(6 81 61)
+		#(7 73 57)
+		#(8 75 54)
+		#(9 86 32)
+		#(10 84 64)
+		#(11 91 59)
+		#(12 88 73)
+		#(13 70 59)
+		#(14 61 59)
+		#(15 64 55)
+		#(16 79 59)
+		#(17 81 57)
+		#(18 82 52)
+		#(19 81 61)
+		#(20 84 57)
+		#(21 86 59)
+		#(22 90 64)
+		#(23 90 68)
+		#(24 90 77)
+		#(25 90 72)
+		#(26 97 64)
+		#(27 91 72)
+		#(28 84 68)
+		#(29 88 66)
+		#(30 90 45)))
 
 (min-spread-id entry-set)
