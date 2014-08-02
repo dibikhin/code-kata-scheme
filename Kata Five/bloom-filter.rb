8d0cee8e4445f7c4609b30b43aa3c31481c45f63
dibikhin <dibikhin@gmail.com>
2014-08-02 21:40:05 +0400

    draft

---
+++ Kata Five/bloom-filter.rb
@@ -0,0 +1,18 @@
+﻿class BloomFilter
+	def initialize(file_name)
+		@bit_vector = load_filter(file_name)
+	end
+	
+	def self.load_filter(file_name)
+	end
+
+	def compute_bit_vector(word_list_file_name)
+	end
+
+	def refresh_bit_vector(new_bit_vector)
+		@bit_vector = new_bit_vector
+	end
+
+	def check_word(word)
+	end
+end
---
+++ Kata Five/spellchecker.rb
@@ -0,0 +1,29 @@
+﻿require 'optparse'
+
+ARGV << '-h' if ARGV.empty?
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: spellchecker.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-f", "--file", String, "File name") do |filename|
+    options[:filename] = filename
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
+
+p options
+#p ARGV
+
+
+
+
+#filter = BloomFilter.new()
