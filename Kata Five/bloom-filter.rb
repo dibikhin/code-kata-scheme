b1e70079eac408a9d19822f4930909e1b4eaebfa
dibikhin <dibikhin@gmail.com>
2014-08-03 21:53:16 +0400

    interface added

--- Kata Five/bloom-filter.rb
+++ Kata Five/bloom-filter.rb
@@ -1,18 +1,13 @@
 ﻿class BloomFilter
-	def initialize(file_name)
-		@bit_vector = load_filter(file_name)
-	end
-	
-	def self.load_filter(file_name)
-	end
-
-	def compute_bit_vector(word_list_file_name)
+	def refresh_vector(dictionary_file_name, vector_file_name)
+		dictionary = load_dictionary(dictionary_file_name)
+		vector = compute_vector(dictionary)
+		save_vector(vector)
 	end
 
-	def refresh_bit_vector(new_bit_vector)
-		@bit_vector = new_bit_vector
-	end
-
-	def check_word(word)
+	def check_word(word, vector_file_name)
+		vector = load_vector(vector_file_name)
+		word_hash = compute_hash(word)
+		is_in_vector(word_hash, vector)
 	end
 end
